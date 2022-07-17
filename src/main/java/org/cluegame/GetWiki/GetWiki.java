e65e034ff4361525026bab02ff88ac716e46c1eb
Benny Chen <bechen1@berkeley.edu>
2022-07-17 11:09:03 -0700

    selenium set up

---
+++ src/main/java/org/cluegame/GetWiki/GetWiki.java
@@ -0,0 +1,8 @@
+package org.cluegame.GetWiki;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetWiki {
+}
